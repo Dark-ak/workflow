ae3b3a932b441607d1faaa6319fcc598895e0f46
Aakash Karki <ak9004729@gmail.com>
2025-01-02 11:03:29 +0530

    Fixed some issue

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -26,13 +26,7 @@ const Toolbar = ({ id, data, offset }) => {
                     <EditNode id={id} data={data} setEdit={setEdit}/>
                 </div>
                 <div className="bg-white   shadow-lg rounded flex items-center">
-                    <div className="cursor-pointer hover:bg-slate-200 p-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-gray-400">
-                            <path d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z" />
-                            <path d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z" />
-                        </svg>
-
-                    </div>
+                    
                     <div className="cursor-pointer hover:bg-slate-200 p-1" onClick={() => setEdit(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-gray-400">
                             <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
